e6df0d0193f6e3b1cae32a5ee637710a0ecc5774
Nadish Sood <nadishsood@gmail.com>
2020-05-22 13:22:17 -0400

    Upto section 14

---
+++ src/actions/index.js
@@ -0,0 +1,28 @@
+import jsonPlaceHolder from './../apis/jsonPlaceholder';
+
+// export const fetchPosts =  () => {
+
+//     return async (dispatch)=>{
+//         const response = await jsonPlaceHolder.get("/posts");
+//         dispatch({type: 'FETCH_POSTS', payload: response });
+//     };
+// };
+
+
+//refactor to es6 removed braces and all
+
+
+export const fetchPosts = () => async dispatch => {
+    const response = await jsonPlaceHolder.get(`posts`);
+    dispatch({type: 'FETCH_POSTS', payload: response});
+}
+
+
+
+
+
+ 
+
+
+
+
